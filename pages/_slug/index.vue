26882c506d8e69178767f1f0647f523f226ae80c
hdyk1119 <knd.hdyk1119@gmail.com>
2020-12-04 12:15:41 +0900

    commit

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
-  ul
-    li(
-      v-for="content in contents"
-      :key="content.id")
-      nuxt-link(:to="`/${content.id}`") {{ content.title }}
+  main.main
+    h1.title {{ title }}
+    p.publishedAt {{ publishedAt }}
+    .post(v-html="body")
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
-  async asyncData() {
+  async asyncData({ params }) {
     const { data } = await axios.get(
-      'https://hdyk1119.microcms.io/api/v1/blog',
+      `https://hdyk1119.microcms.io/api/v1/blog/${params.slug}`,
       {
         headers: {'X-API-KEY': 'c5a388af-bbc9-4a35-9b4d-647b8615c4d8'}
       }
@@ -20,7 +20,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-
-</style>
